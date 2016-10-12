c17bf5831ab32ddce980f45d67e13b407de3f23f
Gabriel Charriez <gabrielc0464@hstat.org>
2016-10-12 01:23:34 +0000

    easy hw done

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,34 @@
-def division
+def division(a,b)
+    a.to_i/b.to_i
     
 end
+division(4,2)
 
-def argue
+def argue(bee)
+    return bee
   
 end
 
-def greeting
+def greeting(d,e)
 
 end
 
-def return_a_value
+def return_a_value()
+    return "Nice"
     
 end
+return_a_value()
 
-def pizza_party
+def pizza_party(cheese="cheese",*p)
+  return cheese 
+   return *p 
+     
+end
+pizza_party("cheese","pepperroni")
+
+
+def whisper(a)
+    return a.downcase
     
 end
-
-def whisper
-    
-end
+whisper("KOBY")
